92b81cefc85eb55484c0d606a7652dfdcf02af05
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2020-07-27 18:35:03 -0400

    Modified client keys

--- config/awesome/lib/keys/client.lua
+++ config/awesome/lib/keys/client.lua
@@ -22,6 +22,137 @@ local resize_inc = 5
 local min_width = 12
 local min_height = 20
 
+-- Functions
+local function focus_tiled_master(c)
+    local s = c.screen
+    local tiled = s:get_tiled_clients(false)
+    local first_tiled = tiled[1]
+
+    awful.client.focus.byidx(0, first_tiled)
+end
+
+local function focus_next_tiled(c)
+    local s = c.screen
+    local tiled = s:get_tiled_clients(false)
+    if #tiled > 1 then
+        local n = awful.client.next(1)
+        while true do
+            if not n.floating then break end
+            n = awful.client.next(1, n)
+        end
+        awful.client.focus.byidx(0, n)
+    end
+end
+
+local function focus_prev_tiled(c)
+    local s = c.screen
+    local tiled = s:get_tiled_clients(false)
+    if #tiled > 1 then
+        local p = awful.client.next(-1)
+        while true do
+            if not p.floating then break end
+            p = awful.client.next(1, p)
+        end
+        awful.client.focus.byidx(0, p)
+    end
+end
+
+local function swap_tiled_master(c)
+    local s = c.screen
+    local tiled = s:get_tiled_clients(false)
+    local first_tiled = tiled[1]
+
+    c:swap(first_tiled)
+end
+
+local function swap_next_tiled(c)
+    local s = c.screen
+    local tiled = s:get_tiled_clients(false)
+    if #tiled > 1 then
+        local n = awful.client.next(1)
+        while true do
+            if not n.floating then break end
+            n = awful.client.next(1, n)
+        end
+        c:swap(n)
+    end
+end
+
+local function swap_prev_tiled(c)
+    local s = c.screen
+    local tiled = s:get_tiled_clients(false)
+    if #tiled > 1 then
+        local p = awful.client.next(-1)
+        while true do
+            if not p.floating then break end
+            p = awful.client.next(1, p)
+        end
+        c:swap(p)
+    end
+end
+
+local function move_floating_left(c)
+    c.x = c.x - reposition_inc
+    awful.placement.no_offscreen(c)
+end
+
+local function move_floating_down(c)
+    c.y = c.y + reposition_inc
+    awful.placement.no_offscreen(c)
+end
+
+local function move_floating_up(c)
+    c.y = c.y - reposition_inc
+    awful.placement.no_offscreen(c)
+end
+
+local function move_floating_right(c)
+    c.x = c.x + reposition_inc
+
+    awful.placement.no_offscreen(c)
+end
+
+local function shrink_floating_left(c)
+    local max_width = c.screen.geometry.width
+    local new = c.width - resize_inc
+
+    if c.floating and new >= min_width and new <= max_width then c.width = new end
+end
+
+local function grow_floating_down(c)
+    local max_height = c.screen.geometry.height
+    local new = c.height + resize_inc
+
+    if c.floating and new >= min_height and new <= max_height then c.height = new end
+end
+
+local function shrink_floating_up(c)
+    local max_height = c.screen.geometry.height
+    local new = c.height - resize_inc
+
+    if c.floating and new >= min_height and new <= max_height then c.height = new end
+end
+
+local function grow_floating_right(c)
+    local max_width = c.screen.geometry.width
+    local new = c.width + resize_inc
+
+    if c.floating and new >= min_width and new <= max_width then c.width = new end
+end
+
+local function focus_next_floating(c)
+    local s = c.screen
+    local cls = s.clients
+    if #cls > 1 then
+        local n = awful.client.next(1)
+        for i = 1,#cls,1 do
+            n = awful.client.next(1, n)
+            if n.floating then break end
+        end
+        awful.client.focus.byidx(0, n)
+    end
+end
+
 local function h_key(c)
     if c.floating then
         c.x = c.x - reposition_inc
@@ -159,54 +290,54 @@ end
 
 local clientkeys = gears.table.join(
     -- Tilled/stacked window controls
-    -- Relative controls; key will act different depending on the floating context.
-    -- Covers both the vim direction keys of hjkl and standard left, up, down and right.
-    awful.key({ super }, "h", function(c) h_key(c) end,
+    -- Works only for tiled or stacked clients
+    -- Covers both the vi direction keys of hjkl and standard left, up, down and right.
+    awful.key({ super }, "h", function(c) if not c.floating then focus_tiled_master(c) end end,
+        { description = "Focus tiled master", group = "Client" }),
+
+    awful.key({ super, shift }, "h", function(c) if not c.floating then swap_tiled_master(c) end end,
+        { description = "Swap with tiled master", group = "Client"}),
+
+    awful.key({ super }, "j", function(c) if not c.floating then focus_next_tiled(c) end end,
+        { description = "Focus next by index", group = "Client"}),
+
+    awful.key({ super, shift }, "j", function(c) if not c.floating then swap_next_tiled(c) end end,
+        { description = "Focus next by index", group = "Client"}),
+
+    awful.key({ super }, "k", function(c) if not c.floating then focus_prev_tiled(c) end end,
+        { description = "Focus previous by index", group = "Client"}),
+
+    awful.key({ super, shift }, "k", function(c) if not c.floating then swap_prev_tiled(c) end end,
+        { description = "Focus previous by index", group = "Client"}),
+
+    awful.key({ super }, "l", function(c) if not c.floating then focus_next_floating(c) end end,
+        { description = "Focus next floating client", group = "Client" }),
+
+    awful.key({ super, shift }, "l", awful.client.floating.toggle,
+        { description = "Toggle floating", group = "Client" }),
+
+    awful.key({ super }, "Left", function(c) if not c.floating then focus_tiled_master(c) end end,
         { description = "Focus first master", group = "Client" }),
 
-    awful.key({ super, shift }, "h", function(c) h_shift_key(c) end,
+    awful.key({ super, shift }, "Left", function(c) if not c.floating then focus_tiled_master(c) end end,
         { description = "Swap with master", group = "Client"}),
 
-    awful.key({ super }, "j", function(c) j_key(c) end,
+    awful.key({ super }, "Down", function(c) if not c.floating then focus_next_tiled(c) end end,
         { description = "Focus next by index", group = "Client"}),
 
-    awful.key({ super, shift }, "j", function(c) j_shift_key(c) end,
+    awful.key({ super, shift }, "Down", function(c) if not c.floating then swap_next_tiled(c) end end,
         { description = "Focus next by index", group = "Client"}),
 
-    awful.key({ super }, "k", function(c) k_key(c) end,
+    awful.key({ super }, "Up", function(c) if not c.floating then focus_prev_tiled(c) end end,
         { description = "Focus previous by index", group = "Client"}),
 
-    awful.key({ super, shift }, "k", function(c) k_shift_key(c) end,
+    awful.key({ super, shift }, "Up", function(c) if not c.floating then swap_prev_tiled(c) end end,
         { description = "Focus previous by index", group = "Client"}),
 
-    awful.key({ super }, "l", function(c) l_key(c) end,
+    awful.key({ super }, "Right", function(c) if not c.floating then focus_next_floating(c) end end,
         { description = "Focus last slave", group = "Client" }),
 
-    awful.key({ super, shift }, "l", function(c) l_shift_key(c) end,
-        { description = "Focus first slave", group = "Client" }),
-
-    awful.key({ super }, "Left", function(c) h_key(c) end,
-        { description = "Focus first master", group = "Client" }),
-
-    awful.key({ super, shift }, "Left", function(c) h_shift_key(c) end,
-        { description = "Swap with master", group = "Client"}),
-
-    awful.key({ super }, "Down", function(c) j_key(c) end,
-        { description = "Focus next by index", group = "Client"}),
-
-    awful.key({ super, shift }, "Down", function(c) j_shift_key(c) end,
-        { description = "Focus next by index", group = "Client"}),
-
-    awful.key({ super }, "Up", function(c) k_key(c) end,
-        { description = "Focus previous by index", group = "Client"}),
-
-    awful.key({ super, shift }, "Up", function(c) k_shift_key(c) end,
-        { description = "Focus previous by index", group = "Client"}),
-
-    awful.key({ super }, "Right", function(c) l_key(c) end,
-        { description = "Focus last slave", group = "Client" }),
-
-    awful.key({ super, shift }, "Right", function(c) l_shift_key(c) end,
+    awful.key({ super, shift }, "Right", awful.client.floating.toggle,
         { description = "Focus first slave", group = "Client" }),
 
     -- Absolute keys; these operate on the whole current client list,
@@ -244,26 +375,7 @@ local clientkeys = gears.table.join(
     awful.key({ super }, "BackSpace", function (c) c:kill() end,
               {description = "Close", group = "Client"}),
 
-    -- Floating windows controls
-    awful.key({ super }, "space",
-        function(c)
-            local s = c.screen
-            local other = not c.floating
-            local cls = s.clients
-            if #cls > 1 then
-                local n = awful.client.next(1)
-                for i = 1,#cls,1 do
-                    n = awful.client.next(1, n)
-                    if n.floating == other then break end
-                end
-                awful.client.focus.byidx(0, n)
-            end
-        end,
-              {description = "toggle floating", group = "Client"}),
-
-    awful.key({ super, "Control" }, "space",  awful.client.floating.toggle,
-              {description = "Toggle floating", group = "Client"}),
-
+    -- Floating window controls
     awful.key({ super }, "Prior",
         function(c)
             if c.floating then
