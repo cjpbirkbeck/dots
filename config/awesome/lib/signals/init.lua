bea9afa0fc8f3c09f6c08e612eb96dbbc590e3d8
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2020-08-07 08:55:51 -0400

    Fixed delete AwesomeWM signals

--- config/awesome/lib/signals/init.lua
+++ config/awesome/lib/signals/init.lua
@@ -66,3 +66,6 @@ client.connect_signal("manage", function(c)
         c.icon = new_icon._native
     end
 end)
+
+client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
