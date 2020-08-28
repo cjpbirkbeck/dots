9df29603f701fd35fb5a2d873f5d6909c60f0498
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2020-08-28 00:44:26 -0400

    Changed line in rofi-prompt

--- home/config/awesome/bin/rofi-prompt.sh
+++ home/config/awesome/bin/rofi-prompt.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
-# TODO: Why does this only work with zsh?
+# Display a dialog box with rofi's dmenu mode. First argument is the prompt, second is the command to be executed.
 
 printf %s "Yes|No" | rofi -dmenu -sep "|" -p "${1}" | { read reply && test "$reply" = "Yes" && shift; ${@} }
