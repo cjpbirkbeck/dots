81ed3981531f6a02b339abc5db31c1203cdb2bd3
Christopher Birkbeck <cjpbirkbeck@gmail.com>
2020-12-01 08:28:26 -0500

    Fixed peekat and find-file scripts
    peekat funtions better, but still lacks a good epub textual preview.
    find-file is revised to look cleaner.

--- home/config/awesome/bin/find-file.sh
+++ home/config/awesome/bin/find-file.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 # Search through files and open them in a new window, as specified by either the extension or mime type.
 
-selected="$(fd --type file "\.(ogg|mp[3-4]|wma|webm|txt|md|markdown|org|epub|pdf|ps|djvu|png|jpe?g|gif|bmp|avi|mkv)$" $HOME/{Audio,Documents,Images,Videos,Downloads} | fzf --prompt="Files> " --reverse --preview="$HOME/.local/bin/peekat -F -- {} " --preview-window=right:wrap)"
+FILE_EXTS="\.(ogg|mp[3-4]|wma|webm|txt|md|markdown|org|epub|pdf|ps|djvu|png|jpe?g|gif|bmp|avi|mkv)$"
+
+selected="$(fd --type file "$FILE_EXTS" $HOME/{Audio,Documents,Images,Videos,Downloads} | \
+    fzf --prompt="Files> " --reverse --preview="$HOME/.local/bin/peekat {} " --preview-window=right:wrap)"
 
 test -n "$selected" && "$HOME/.config/awesome/bin/open-file.sh" "$selected"
